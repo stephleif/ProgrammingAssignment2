3cba149cf1cf54b14064d025dc700d578501e9cf
Stephanie Leif <stephleif@gmail.com>
2020-11-23 16:25:44 -0800

    These are sample matrix for testing.  They came from discussion forum week 3

---
+++ testcachematrix.R
@@ -0,0 +1,10 @@
+## Code to test cachematrix
+## from discussion forum week 3
+## Matrices for testing the R functions 
+
+m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+
+I2 <- matrix(c(1,0,0,1), nrow = 2, ncol = 2)
+n1 <- matrix(c(6,2,8,4), nrow = 2, ncol = 2)
+## n1 is inverse m1
+n2 <- matrix(c(5/8, -1/8, -7/8, 3/8), nrow = 2, ncol = 2)
